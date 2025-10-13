6174e3bccb5e5f94775530ee641bb2ffdf0d313f
Luckywolf82 <trygvetrygg@hotmail.com>
2025-10-13 14:18:53 +0200

    Create validate.js

---
+++ netlify/functions/_lib/validate.js
@@ -0,0 +1,11 @@
+// _lib/validate.js
+function isNonEmptyString(s) {
+  return typeof s === 'string' && s.trim().length > 0;
+}
+function isOptionalString(s) {
+  return s == null || typeof s === 'string';
+}
+function isOneOf(v, arr) {
+  return arr.includes(v);
+}
+module.exports = { isNonEmptyString, isOptionalString, isOneOf };
